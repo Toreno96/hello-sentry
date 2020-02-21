e229dc703bdf7d1bb9701fccef8a0d10194b3447
Daniel <toreno96@gmail.com>
2020-02-21 16:09:42 +0100

    Reimplement settings using Pydantic

--- app/settings.py
+++ app/settings.py
@@ -1,10 +1,16 @@
 import logging
-import os
 
-SENTRY_SDK_DSN = os.getenv("SENTRY_SDK_DSN")
+from pydantic import BaseSettings
 
 logging.basicConfig(
     datefmt="%F %T",
     format="[%(asctime)s] %(levelname)s:%(module)s:%(lineno)d: %(message)s",
     level=logging.DEBUG,
 )
+
+
+class EnvironmentSettings(BaseSettings):
+    SENTRY_SDK_DSN: str  # https://docs.sentry.io/error-reporting/quickstart/?platform=python#configure-the-sdk
+
+
+environment_settings = EnvironmentSettings()
