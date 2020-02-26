Toreno96/hello-sentry

commit e334f1b28535e76f7a9d23f5e3f1b4c6f334136d

Improve print_release_version.py dependencies
Make SENTRY_SDK_DSN environment variable not required for it.

app/__metadata__.py
@@ -1,0 +1,3 @@
+__version__ = "0.4.1"
+__project_name__ = "hello-sentry"
+__release__ = f"{__project_name__}@{__version__}"

app/__init__.py
@@ -1,1 +1,1 @@
-from .__version__ import __version__  # noqa
+from .__metadata__ import __version__  # noqa

app/__version__.py
@@ -1,1 +1,0 @@
-__version__ = "0.3.0"

app/settings.py
@@ -2,8 +2,6 @@ import logging
 
 from pydantic import BaseSettings
 
-from .__version__ import __version__
-
 logging.basicConfig(
     datefmt="%F %T", format="[%(asctime)s] %(levelname)s:%(module)s:%(lineno)d: %(message)s", level=logging.DEBUG,
 )
@@ -14,7 +12,3 @@ class EnvironmentSettings(BaseSettings):
 
 
 environment_settings = EnvironmentSettings()
-
-
-PROJECT_NAME = "hello-sentry"
-RELEASE = f"{PROJECT_NAME}@{__version__}"

print_release_version.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 
-from app import settings
+from app import __metadata__
 
-print(settings.RELEASE)
+print(__metadata__.__release__)

commit efab558f9927ac77937b507a7c62cfcded6eba1a

Fix error
Introduced in e334f1b28535e76f7a9d23f5e3f1b4c6f334136d.

app/__metadata__.py
@@ -1,3 +1,3 @@
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 __project_name__ = "hello-sentry"
 __release__ = f"{__project_name__}@{__version__}"

app/app.py
@@ -1,8 +1,9 @@
 import sentry_sdk
 
+import __metadata__
 import settings
 
-sentry_sdk.init(settings.environment_settings.SENTRY_SDK_DSN, release=settings.RELEASE)
+sentry_sdk.init(settings.environment_settings.SENTRY_SDK_DSN, release=__metadata__.__release__)
 
 
 def main():